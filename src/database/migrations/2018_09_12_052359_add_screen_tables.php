040a976a8c846890ac227f1223ed944abdbcccb9
Namal Attanayake <namal@parallax.lk>
2018-09-12 11:09:12 +0530

    adding modules and screens for permissions

---
+++ src/database/migrations/2018_09_12_052359_add_screen_tables.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddScreenTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableNames = config('permission.table_names');
+
+        /**
+         * This table is used for categorizing the permissions.
+         */
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->integer('order', FALSE, TRUE)->nullable();
+            $table->unsignedTinyInteger('active', FALSE)->default(1);
+            $table->timestamps();
+        });
+
+        Schema::create('screens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('order', FALSE, TRUE)->nullable();
+            $table->unsignedTinyInteger('active', FALSE)->default(1);
+
+            $table->integer('module_id', FALSE, TRUE);
+            $table->foreign('module_id')
+                ->references('id')
+                ->on('screens')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::table($tableNames['permissions'], function (Blueprint $table) {
+            $table->unsignedInteger('screen_id', FALSE)->after('guard_name');
+            $table->foreign('screen_id')
+                ->references('id')
+                ->on('screens')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
