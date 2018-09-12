386216bec2b9400f9b89c78279b0844f635f644f
Namal Attanayake <namal@parallax.lk>
2018-09-12 16:45:44 +0530

    db seed syntax error fixed

--- src/commands/GenerateRolesAndPermissionsModule.php
+++ src/commands/GenerateRolesAndPermissionsModule.php
@@ -52,8 +52,8 @@ class GenerateRolesAndPermissionsModule extends Command
 
         $this->call('make:auth');
         $this->call('migrate');
-        $this->call('db:seed --class=UserPermissionSeed');
-        $this->call('db:seed --class=UserRolePermissionSeed');
+        $this->call('db:seed', ['--class' => 'UserPermissionSeed']);
+        $this->call('db:seed', ['--class' => 'UserRolePermissionSeed']);
 
         $this->info('====================================');
 
