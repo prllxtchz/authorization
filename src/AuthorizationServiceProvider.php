4eb2078a9655e37e918404df655493c6af846380
Namal Attanayake <namal@parallax.lk>
2018-09-12 11:26:25 +0530

    publish migrations

--- src/AuthorizationServiceProvider.php
+++ src/AuthorizationServiceProvider.php
@@ -8,19 +8,19 @@ class AuthorizationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'authorization');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views', 'authorization');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
         $this->publishes([
-            __DIR__.'/views' => resource_path('views'),
-            __DIR__.'/database/seeds' => database_path('seeds'),
+            __DIR__ . '/views' => resource_path('views'),
+            __DIR__ . '/database/migrations' => database_path('migrations'),
+            __DIR__ . '/database/seeds' => database_path('seeds'),
         ]);
     }
 
     public function register()
     {
-        
+
     }
 }
